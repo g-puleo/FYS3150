92df7bd66625dcd0fed8756411cb9bb77cf369f9
giammy00 <74814411+giammy00@users.noreply.github.com>
2023-02-08 16:58:32 +0100

    updated potential

--- PROJECT 5/PotentialFiles/potential_maker.py
+++ PROJECT 5/PotentialFiles/potential_maker.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 M = 201
-
+h = 0.005
 # V = np.zeros((M,M))
 # # Double slit
 # for i in range(98, 102, 1): 
@@ -40,12 +40,26 @@ M = 201
 		
 # np.savetxt("single_slit.dat", V) 
 
-V = np.zeros((M,M))
-# Wall
-for i in range(98, 102, 1): 
-	for j in range(M): 
-		V[i][j] = 1
+# V = np.zeros((M,M))
+# # Wall
+# for i in range(98, 102, 1): 
+# 	for j in range(M): 
+# 		V[i][j] = 1
 		
-np.savetxt("barrier.dat", V) 
+# np.savetxt("barrier.dat", V) 
 
+V = np.zeros((M,M))
+
+omega=1;
+m = 1;
+
+for i in range(M):
+
+	for j in range(M):
+		
+		V[i][j]  = 0.5*m*omega**2*((i*h-0.5)**2 + (j*h-0.5)**2)
+		print(V[i][j], end=' ')
+	print("\n")
+
+np.savetxt("harmonic.dat", V)
 		
